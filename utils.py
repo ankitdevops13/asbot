5047afc605b7d6a8541e37ad648b8eedb516f9f7
AnkitShakya98 <vimla3119@gmail.com>
2025-07-02 04:10:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -100,6 +100,6 @@ async def progress_bar(current, total, reply, start):
             progress_bar = completed_symbol * completed_length + remaining_symbol * remaining_length
             
             try:
-                await reply.edit(f'`╭──⌯════𝐁𝐨𝐭 𝐒𝐭𝐚𝐭𝐢𝐜𝐬═════⌯──╮\n├⚡ {progress_bar}\n├⚙️ Progress ➤ | {perc} |\n├🚀 Speed ➤ | {sp} |\n├📟 Processed ➤ | {cur} |\n├🧲 Size ➤ | {tot} |\n├🕑 ETA ➤ | {eta} |\n╰─══✨🦋 RAO SAHAB ✅ 🦋✨══─╯`') 
+                await reply.edit(f'`╭──⌯════ 𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴 📤 ═════⌯──╮\n├⚡ {progress_bar}\n├⚙️ Progress ➤ | {perc} |\n├🚀 Speed ➤ | {sp} |\n├📟 Processed ➤ | {cur} |\n├🧲 Size ➤ | {tot} |\n├🕑 ETA ➤ | {eta} |\n╰─══✨ 𝐀𝐧𝐤𝐢𝐭 𝐒𝐡𝐚𝐤𝐲𝐚🇮🇳 ✨══─╯`') 
             except FloodWait as e:
                 time.sleep(e.x)
