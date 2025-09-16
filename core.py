6f76f7ce0e8569b768cf83f15420263f376ad1e7
AnkitShakya98 <vimla3119@gmail.com>
2025-09-16 18:32:15 +0530

    Update core.py

--- core.py
+++ core.py
@@ -233,7 +233,7 @@ async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
 
 async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
-    await prog.delete (True)
+#   await prog.delete (True)
     reply = await m.reply_text(f"**Uploading ..🚀..** - `{name}`\n╰────⌈**𝐀𝐍𝐊𝐈𝐓❤️**⌋────╯")
     try:
         if thumb == "no":
